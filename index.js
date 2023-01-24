655dbf6edf7dd0643cf6a807cd307469832ef437
RPIUdishan <it18082234@my.sliit.lk>
2023-01-24 14:49:51 +0530

    mongo db connection

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express  from "express";
 import dotenv from "dotenv"
-// import mongoose from "mongoose";
+import mongoose from "mongoose";
 // import cookieParser from "cookie-parser";
 // import cors from "cors"
 
@@ -11,5 +11,25 @@ dotenv.config();
 //db connection
 app.listen(process.env.PORT, () => {
     console.log("Connected to the Backend...")
-    // connect()
+    connect()
 });
+
+//getting connection on mongo db atlas
+const connect = async () => {
+    try{
+        await mongoose.connect(process.env.DB_CONNECTION_STRING)
+        console.log("Connected to the Database...")
+    }catch(err){
+        throw err;
+    }
+};
+
+//event listner for db connection
+mongoose.connection.on('connected', () => {
+    console.log("Connected")
+})
+
+//event listner for db disconnection
+mongoose.connection.on('disconnected', () => {
+    console.log("Disconnected")
+})
