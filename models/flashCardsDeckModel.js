ea7a410d7a9cf2fe5f52157aea8c8a1984926475
RPIUdishan <it18082234@my.sliit.lk>
2023-01-25 09:38:13 +0530

    initial structure added

---
+++ models/flashCardsDeckModel.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+const flashCardsDeckSchema = new Schema({
+  title:  {
+    type: String,
+    required: true
+  },
+  subject: {
+    type: String,
+    required: true 
+  },
+  color: {
+    type: String,
+    required: true 
+  },
+  noOfCards: {
+    type: Number,
+    required: true 
+  }
+
+});
+
+export default model("FlashCardsDeck", flashCardsDeckSchema)
